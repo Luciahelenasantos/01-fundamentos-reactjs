55b8d6892ce70673dfdda03b1ad2f2a7725ac03b
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2024-05-11 15:02:29 -0300

    Add component Sidebar App.jsx App.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { Post } from "./Post"
 import { Header } from "./components/Header"
+import { Post } from "./Post"
+
+import styles from './App.module.css'
 
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post 
-        author="Nicole Bidigaray" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae tempora laborum recusandae sunt quasi dicta mollitia nam illo, aliquam suscipit." 
-      />
-      <Post
-         author="Marcelo Tedesco"
-         content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Nicole Bidigaray" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae tempora laborum recusandae sunt quasi dicta mollitia nam illo, aliquam suscipit." 
+          />
+          <Post
+            author="Marcelo Tedesco"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
